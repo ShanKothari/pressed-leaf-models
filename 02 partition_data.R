461b9a502ef367e877a674abd6acefb0473cd626
Shan Kothari <quercusacerifolia@gmail.com>
2022-07-12 17:19:19 -0400

    add documentation

--- 02 partition_data.R
+++ 02 partition_data.R
@@ -2,6 +2,12 @@ setwd("C:/Users/kotha020/Dropbox/TraitModels2018/HerbariumPaper/")
 library(spectrolab)
 library(caret)
 
+## here we separate data between calibration and validation
+## subsets so that the same samples are used for validation
+## in analyses of fresh, pressed, and ground leaves
+## so that comparisons of accuracy are more interpretable
+## (no variation due to the caprices of random cal/val splits)
+
 ######################################################
 ## read data
 
@@ -16,10 +22,13 @@ fresh_meta<-meta(fresh_spec_all)
 pressed_meta<-meta(pressed_spec_all)
 ground_meta<-meta(ground_spec_all)
 
+## archiving data!
 # write.csv(as.data.frame(fresh_spec_all),"ProcessedSpectralData/fresh_spec_avg.csv",row.names = F)
 # write.csv(as.data.frame(pressed_spec_all),"ProcessedSpectralData/pressed_spec_avg.csv",row.names = F)
 # write.csv(as.data.frame(ground_spec_all),"ProcessedSpectralData/ground_spec_avg.csv",row.names = F)
 
+## the few lines below are just to help count numbers of samples
+## per project, growth form, or species
 fp_merge<-merge(fresh_meta,pressed_meta,by="ID",all=T)
 fp_merge$GrowthForm.x[is.na(fp_merge$GrowthForm.x)]<-fp_merge$GrowthForm.y[is.na(fp_merge$GrowthForm.x)]
 fp_merge$Species.x[is.na(fp_merge$Species.x)]<-fp_merge$Species.y[is.na(fp_merge$Species.x)]
@@ -32,15 +41,17 @@ table(fp_merge$Species.x)[order(names(table(fp_merge$Species.x)))]
 
 ## which data are in all three data sets?
 fresh_spec_int<-fresh_spec_all[Reduce(intersect,list(names(fresh_spec_all),
-                                                          names(pressed_spec_all),
-                                                          names(ground_spec_all)))]
+                                                     names(pressed_spec_all),
+                                                     names(ground_spec_all)))]
 
+## choose validation samples from among those shared in common
 test_sample_fresh <- createDataPartition(
   y = meta(fresh_spec_int)$GrowthForm,
   p = .25,
   list = FALSE
 )
 
+## do the splits
 fresh_spec_all_train<-fresh_spec_all[-test_sample_fresh,]
 fresh_spec_all_test<-fresh_spec_all[test_sample_fresh,]
 test_names<-names(fresh_spec_all_test)
